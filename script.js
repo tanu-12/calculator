6c7fa24efd122ce99fade612a912858ea31f2663
Tanu <tanupandey291099@gmail.com>
2024-04-25 16:53:28 +0530

    added number updating function

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const 
+const nums = document.querySelectorAll(".num");
+console.log(nums);
+const op = document.querySelectorAll(".operator");
+console.log(op);
+const equal = document.querySelector(".equals");
+console.log(equal);
+const displayVal = document.querySelector(".displayVal");
+const cV = document.querySelector(".currentVal");
+let operator, prevVal, currentVal;
+
+prevVal = 0;
+currentVal = 0;
+operator = "";
+function updateNumber(n) {
+    currentVal = n;
+    displayVal.textContent += currentVal;
+    cV.textContent = currentVal;
+}
+
+nums.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        console.log("number was clicked!!!!");
+        updateNumber(e.target.textContent);
+
+
+
+    });
+});
+
